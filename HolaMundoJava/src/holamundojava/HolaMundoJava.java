70de7d4377a3b8b1684263b00f8c9816672f8e75
Juan Casanova <juancasanovagago@gmail.com>
2021-11-22 19:23:09 +0100

    Proyecto HolaMundoJava vacío

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * 
+ */
+
+package holamundojava;
+
+/*
+ * @author Juan Casanova Gago <casanovagagojuan@gmail.com>
+ * @company DAW
+ * @version 1.0
+ * @date 22 nov. 2021 19:05:49
+ */
+public class HolaMundoJava {
+    public static void main(String[] args) {
+       
+    }
+
+}
